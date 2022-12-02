bd5884bb727246ed82eb5937085fb456f506f782
Samaoo <sam.verhaegen2003@gmail.com>
2022-12-02 17:46:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CollectionForm from './Form'
 
 class App extends Component{
 
-    API_URL="http://backend:8080"
+    API_URL="http://193.191.176.107:8080/"
 
     state = {
       products: [],
